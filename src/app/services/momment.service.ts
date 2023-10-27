a790832d512dc0ae72ba0f5fcd610200de24dd02
lucas <lucas.goliveirass@gmail.com>
2023-10-26 21:52:27 -0300

    Primeiro commit do projeto Moments

---
+++ src/app/services/momment.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Moment } from '../Moment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MommentService {
+
+  constructor() { }
+}
